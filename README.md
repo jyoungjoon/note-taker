eb6dc935d3a9887d50f36dbf51b7a69bccfe608a
Young Jang <jyoungjoon@gmail.com>
2023-06-03 14:47:50 -0700

    added  README and LICENSE files

---
+++ README.md
@@ -0,0 +1,87 @@
+![license](https://img.shields.io/badge/license-ISC-blue.svg) ![logo-creator](https://img.shields.io/github/languages/count/jyoungjoon/note-taker) ![logo-creator](https://img.shields.io/github/languages/top/jyoungjoon/note-taker)
+
+---
+
+# Note Taker Application
+
+## Description
+
+![Screenshot](./public/assets/images/screenshot.png)
+
+This is a note taking application that allows users to create, save, and delete notes. This application uses an express backend and saves and retrieves note data from a JSON file. This application is deployed on Heroku.
+
+---
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+---
+
+## Installation
+
+Installation is required to run the application locally. To install the application, run the following command:
+
+```bash
+npm install
+```
+
+You can also skip the installation run the application by clicking on the following link: https://youngs-note-taker.herokuapp.com/
+
+---
+
+## Usage
+
+This application is deployed on Heroku. You can access the application by clicking on the following link: https://youngs-note-taker.herokuapp.com/
+
+You can also run the application locally by running the following command:
+
+```bash
+npm start
+```
+
+Then, open your browser and go to http://localhost:3001/
+
+---
+
+## License
+
+This project is licensed under the ISC license. Please see LICENSE.md for more information.
+
+---
+
+## Credits
+
+Starter code was provided by the University of California Berkeley Coding Bootcamp. Great staff members and TAs at the bootcamp helped me with this project.
+
+Special thanks to Jili Jiang (a tutor) for helping me with this project! She is a great tutor and I highly recommend her!
+
+---
+
+## Contributing
+
+For contributions, please create a new branch of the project and submit a pull request. Any contributions are greatly appreciated. Issues can be submitted in the issues section of the repo or by emailing me directly (see Questions section below).
+
+---
+
+## Tests
+
+Test feature is not available for this application.
+
+---
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at:
+
+Github: [jyoungjoon](https://github.com/jyoungjoon)
+
+Email: lifescriptified@gmail.com
+
+---
--- server.js
+++ server.js
@@ -1,62 +1,75 @@
-const express = require('express');
-const path = require('path');
-const fs = require('fs');
+// Require necessary modules for the app: express, path, and fs
+const express = require(`express`);
+const path = require(`path`);
+const fs = require(`fs`);
 
+// Setting up PORT; if process.env.PORT is not available, then 3001 will be used
 const PORT = process.env.PORT || 3001;
 
+// Setting up express app
 const app = express();
 
+// Setting up express app to handle data parsing; static files are also served; the middlewares:
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(`public`));
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+// GET routes for HTML files
+app.get(`/`, (req, res) =>
+  res.sendFile(path.join(__dirname, `/public/index.html`))
+);
+app.get(`/notes`, (req, res) =>
+  res.sendFile(path.join(__dirname, `/public/notes.html`))
 );
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-app.get('/api/notes', (req, res) => {
+// GET routes for API files; the data is parsed and sent to the client
+app.get(`/api/notes`, (req, res) => {
   const data = JSON.parse(
-    fs.readFileSync(path.join(__dirname, './db/db.json'))
+    fs.readFileSync(path.join(__dirname, `./db/db.json`))
   );
   res.send(data);
 });
 
-app.post('/api/notes', (req, res) => {
+// POST route for API files
+app.post(`/api/notes`, (req, res) => {
+  // The new note is created and added to the db.json file
   const { title, text } = req.body;
-  const newNote = { title, text, id: Math.random() };
+  const newNote = { title, text, id: Math.trunc(Math.random() * 99999 + 1) };
   const data = JSON.parse(
-    fs.readFileSync(path.join(__dirname, './db/db.json'))
+    fs.readFileSync(path.join(__dirname, `./db/db.json`))
   );
   data.push(newNote);
-
   fs.writeFile(
-    path.join(__dirname, './db/db.json'),
+    path.join(__dirname, `./db/db.json`),
     JSON.stringify(data),
     (err) => (err ? console.error(err) : console.log(`success`))
   );
   res.json(newNote);
 });
 
-app.delete('/api/notes/:id', (req, res) => {
+// DELETE route for API files
+app.delete(`/api/notes/:id`, (req, res) => {
+  // The note is deleted from the db.json file
   const data = JSON.parse(
-    fs.readFileSync(path.join(__dirname, './db/db.json'))
+    fs.readFileSync(path.join(__dirname, `./db/db.json`))
   );
 
-  const newData = data.filter((note) => note.id.toString() !== req.params.id);
+  // The note is deleted from the db.json file with the filter method:
+  const newData = data.filter((note) => note.id !== req.params.id * 1);
 
   fs.writeFile(
-    path.join(__dirname, './db/db.json'),
+    path.join(__dirname, `./db/db.json`),
     JSON.stringify(newData),
     (err) => (err ? console.error(err) : console.log(`success`))
   );
-  res.json(`Success`);
+  res.json(newData);
 });
 
+// Listening on PORT; if message displays, then the server is live!
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
+  console.log(
+    `App listening at ${
+      PORT === 3001 ? `localhost:` : `process.env.PORT:`
+    }${PORT} 🚀`
+  )
 );
